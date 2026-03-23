4cbc03bae436c8ab1905cb65b27abf246df224a5
tata5946 <takuya.t.scottcr120@gmail.com>
2026-03-23 12:03:37 +0900

    Update common.js

--- assets/common.js
+++ assets/common.js
@@ -7,7 +7,7 @@
  */
 
 /* ----- Config ----- */
-const GAS_URL = "https://script.google.com/macros/s/AKfycby5y8eWlowgNaFFezDM-ApclOCsnceNGVAJhEoo4ptBcu95u5ThTCrYUzWi31y_8Kfa/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyY73ta--ltwmcWMM8YhmVkJdvmJvZFPEmypN_hIoZboZg9FgUFvG6lxSN9ZiAmj5cT/exec";
 const GA_MEASUREMENT_ID = "G-FLDX8EB1W8";
 const FAVICON_PATH = "./assets/favicon.svg";
 const DATA_CACHE_KEY = "pokeSleeve:dataCache:v1";
